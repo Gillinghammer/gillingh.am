4104d27b476150713bde42ee7d6b8e6fddb821b6
Gillinghammer <colingillingham@gmail.com>
2017-02-16 16:45:34 +0000

    updated and working form

--- routes/index.js
+++ routes/index.js
@@ -1,48 +1,48 @@
+var nodemailer = require('nodemailer');
 var express = require('express');
 var router = express.Router();
+
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { foo: 'bar' });
 });
 
-router.get('/wirewax', function(req, res, next) {
-  res.render('wirewax', { 
-    company: 'WIREWAX', 
-    location: 'London, UK',
-    url: 'https://www.wirewax.com', 
-    logo: '/images/wwx_logo.png',
-    employed: 'September 2015 - October 2016',
-    description: 'WIREWAX uses advanced machine learning to recognize the objects and people inside video, making it possible for the audience to actively engage and explore video like never before.',
-    thumbnails: ["https://edge-assets.wirewax.com/creativeData/ww_brand-assets/vans_mobile.jpg", "https://edge-assets.wirewax.com/creativeData/ww_brand-assets/coach_mobile.jpg", "https://edge-assets.wirewax.com/creativeData/ww_brand-assets/ri_laptop.jpg"],
-    roles: [{jobTitle: "Product Manager", jobDescription: "Managed the product roadmap, engineering and design teams.", bullets: ["Distilled product requirements and strategy by working closely with the CEO and cofounder.", "Provided my own insight and ideas for improving the user experience of the product.", "Built wireframes to illustrate required functionalities to provide clarity for design and development", "Managed six person developer team using sprints and an agile release schedule", "Jumped into the code-base to make front end improvements"]}]
-  });
-});
+/* Send Email */
+router.post('/', handleSayHello );
 
-router.get('/proleads', function(req, res, next) {
-  res.render('proleads', { 
-    company: 'ProLeads',
-    location: 'San Francisco, CA', 
-    url: 'https://proleads.io', 
-    logo: '/images/proleads_logo.png',
-    employed: 'February 2015 - July 2015',
-    description: 'ProLeads has built the ideal sales automation tool- Allowing small sales teams to craft extremely personal and highly effective emails with very little effort.',
-    thumbnails: ["https://proleads.io/assets/imac-27-proleads-flow-transparent-500x485.png", "http://www.fillmurray.com/300/150", "http://www.fillmurray.com/300/150"],
-    roles: [{jobTitle: "Product Manager", jobDescription: "Managed the product roadmap, engineering and design teams.", bullets: ["Distilled product requirements and strategy by working closely with the CEO and cofounder.", "Provided my own insight and ideas for improving the user experience of the product.", "Built wireframes to illustrate required functionalities to provide clarity for design and development", "Managed six person developer team using sprints and an agile release schedule", "Jumped into the code-base to make front end improvements"]}]
-  });
-});
+var gmailpass = process.env.GMAILPASS;
+console.log("your password from process.env.GMAILPASS is ", gmailpass);
 
-router.get('/tesla', function(req, res, next) {
-  res.render('tesla', { 
-    company: 'Tesla',
-    location: 'Palo Alto, CA', 
-    url: 'https://tesla.com', 
-    logo: '/images/tesla_logo.png',
-    employed: 'June 2008 - September 2013',
-    description: 'Tesla develops advanced battery technology used in building world class electric cars and sophisticated energy storage systems under the leadership of Elon Musk.',
-    thumbnails: ["http://www.fillmurray.com/300/150", "http://www.fillmurray.com/300/150", "http://www.fillmurray.com/300/150"],
-    roles: [{jobTitle: "Product Manager", jobDescription: "Managed the product roadmap, engineering and design teams.", bullets: ["Distilled product requirements and strategy by working closely with the CEO and cofounder.", "Provided my own insight and ideas for improving the user experience of the product.", "Built wireframes to illustrate required functionalities to provide clarity for design and development", "Managed six person developer team using sprints and an agile release schedule", "Jumped into the code-base to make front end improvements"]}]
+function handleSayHello(req, res) {
+
+  var transporter = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+      user: 'colingillingham@gmail.com',
+      pass: gmailpass
+    }
   });
-});
+
+  var text = "hello portugal from \n\n" + req.body.email + "\n\n" + req.body.message;
+  var mailOptions = {
+    from: "blahblah@gmail.com",
+    to: "colingillingham@gmail.com",
+    subject: "Email Example",
+    text: text
+  };
+
+  transporter.sendMail( mailOptions, function(error, info){
+    if(error){
+      console.log(error);
+      res.json({yo: 'error'});
+    } else {
+      console.log('Message sent: ' + info.response);
+      res.redirect('/');
+    }
+  })
+
+}
 
 module.exports = router;
