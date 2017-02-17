f7b95df7ae6495d265f55a48bce2a129d4c8e7d0
Gillinghammer <colingillingham@gmail.com>
2017-02-17 07:59:25 +0000

    loads of imprpveents

--- routes/index.js
+++ routes/index.js
@@ -9,40 +9,4 @@ router.get('/', function(req, res, next) {
   res.render('index', { foo: 'bar' });
 });
 
-/* Send Email */
-router.post('/', handleSayHello );
-
-var gmailpass = process.env.GMAILPASS;
-console.log("\n\nyour password from process.env.GMAILPASS is " + gmailpass);
-
-function handleSayHello(req, res) {
-
-  var transporter = nodemailer.createTransport({
-    service: 'Gmail',
-    auth: {
-      user: 'colingillingham@gmail.com',
-      pass: gmailpass
-    }
-  });
-
-  var text = "hello portugal from \n\n" + req.body.email + "\n\n" + req.body.message;
-  var mailOptions = {
-    from: "blahblah@gmail.com",
-    to: "colingillingham@gmail.com",
-    subject: "Email Example",
-    text: text
-  };
-
-  transporter.sendMail( mailOptions, function(error, info){
-    if(error){
-      console.log(error);
-      res.json({yo: 'error'});
-    } else {
-      console.log('Message sent: ' + info.response);
-      res.redirect('/');
-    }
-  })
-
-}
-
 module.exports = router;
