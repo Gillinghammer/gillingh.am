64c6722effc62e8313537137ec4b3466a83d7ead
Gillinghammer <colingillingham@gmail.com>
2017-03-30 20:09:49 +0100

    restructured layout

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,21 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index');
+  // detect browser language settings
+  var lang = req.headers["accept-language"].substring(0, 2);
+
+  // redirect to portuguese page if pt language is detected
+  if(lang === "pt"){
+    res.redirect('/pt');
+  } else {
+    res.render('index');
+  }
+
+});
+
+/* GET home page. */
+router.get('/pt', function(req, res, next) {
+  res.render('indexpt');
 });
 
 module.exports = router;
